3a09682d2f34243b4fd80c75148b6e7d45360bdd
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-07-15 19:11:37 +0900

    1563185496

---
+++ upload_to_github.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# 引数$1はmain.rbから渡されます
+
+git add docs/geojson/$1.geojson
+git commit -m "add $1"
+git push origin master && firefox -new-tab https://yuukitoriyama.github.io/ryokucha/index.html?q=$1
