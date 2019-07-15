cbc2605aae5e4b6ef539b997de76a7ac62d8b0fc
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-07-16 07:24:12 +0900

    small fix

--- upload_to_github.bash
+++ upload_to_github.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 # 引数$1はmain.rbから渡されます
 
+echo "https://yuukitoriyama.github.io/ryokucha/index.html?q=$1"
+
 git add docs/geojson/$1.geojson
 git commit -m "add $1"
 git push origin master && firefox -new-tab "https://yuukitoriyama.github.io/ryokucha/index.html?q=$1" &
-
-echo "https://yuukitoriyama.github.io/ryokucha/index.html?q=$1"
